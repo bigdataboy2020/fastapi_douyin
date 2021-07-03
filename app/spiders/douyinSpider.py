d302ff5348e156af18fad2e65a5405b685f8f7f3
bigdataboy2020 <63535900+bigdataboy2020@users.noreply.github.com>
2021-07-03 18:49:33 +0800

    新版本抖音无水印解析
    仅供学习交流

--- app/spiders/douyinSpider.py
+++ app/spiders/douyinSpider.py
@@ -1,84 +1,82 @@
 # -*- coding: utf-8 -*-
-# @Time    : 20200529
+# @Time    : 20210702
 # @Author  : 大数据男孩
 # @Blog    ：https://bigdataboy.cn
 
 import requests
-import re
+import re, json
+from requests.utils import unquote
+from utils import log
 
 
-class DYSpider:
+class DYSpider(object):
 
     def __init__(self, share_url):
-        # get 获取3个参数 item_ids mid u_code
-        self.share_url = share_url
-        self.item_ids = ""
-        self.mid = ""
-        self.u_code = ""
+        self.share_url = share_url  # 分享链接
+        self.session = requests.Session()
+        self.video_num = None  # 视频编号
+        self.video_info = None  # 视频完整信息
 
-        # get 获取参数 dytk 后面大括号需要 item_ids
-        self.dytk_url = "https://www.iesdouyin.com/share/video/{}/"
-        self.dytk = ""
+    # 获取真实的视频编号
+    def get_video_num(self) -> bool:
+        s = self.session.get(
+            url=self.share_url,
+            headers={
+                'Host': 'v.douyin.com',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            },
+            allow_redirects=False
 
-        # 获取信息接口 get
-        self.infor_url = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/"
+        )
+        if s.status_code == 302:
+            self.video_num = re.search(r'video/(\d+)/', s.headers.get('Location')).group(1)
+            return True
+        else:
+            return False
 
-        self.headers = {
-            "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
-        }
+    # 获取视频信息
+    def get_video_info(self) -> bool:
+        r = self.session.get(
+            url=f'https://www.douyin.com/video/{self.video_num}',
+            headers={
+                'Host': 'www.douyin.com',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            },
+            params={'previous_page': 'app_code_link'}
+        )
+        r = re.search(r'id="RENDER_DATA" type="application/json">(.+?)</script>', r.text).group(1)
+        # print(unquote(r))
+        self.video_info = json.loads(unquote(r))
+        return True
 
-    def get_imu(self):
-        r = requests.get(url=self.share_url)
-        dytk_data = r.url
-        self.item_ids = re.search(r'video/(.+?)/', dytk_data).group(1)
-        self.mid = re.search(r'mid=(.+?)&', dytk_data).group(1)
-        self.u_code = re.search(r'u_code=(.+?)&', dytk_data).group(1)
-
-    def get_dytk(self):
-        url = self.dytk_url.format(self.item_ids)
-        r = requests.get(url=url, headers=self.headers, params={
-            "region": "CN",
-            "mid": self.mid,
-            "u_code": self.u_code,
-            "titleType": "title",
-            "utm_source": "copy_link",
-            "utm_campaign": "client_share",
-            "utm_medium": "android",
-            "app": "aweme",
-        })
-        self.dytk = re.search(r'dytk: "(.+?)" }\);', r.text)
-
-    def get_infor(self):
-        r = requests.get(url=self.infor_url, headers=self.headers, params={
-            "item_ids": self.item_ids,
-            "dytk": self.dytk,
-        })
-        data_json = r.json()
-        print(r.text)
-        item_list = data_json.get("item_list")[0]
+    def get_info(self):
         # 标题
-        desc = item_list.get("desc")
+        desc = self.video_info.get('C_12').get('aweme').get('detail').get('desc')
         # 原视频
-        video_url = item_list.get("video").get("play_addr").get("url_list")[0].replace("playwm", "play")
+        video_url = "https:" + self.video_info.get('C_12').get('aweme').get('detail').get('video').get('playAddr')[
+            0].get('src')
         # 动图
-        dynamic_cover = item_list.get("video").get("dynamic_cover").get("url_list")
+        dynamic_cover = "https:" + self.video_info.get('C_12').get('aweme').get('detail').get('video').get(
+            'dynamicCover')
         # 静图
-        origin_cover = item_list.get("video").get("origin_cover").get("url_list")
+        origin_cover = "https:" + self.video_info.get('C_12').get('aweme').get('detail').get('video').get('cover')
         return {"title": desc, "img_run": dynamic_cover, "img": origin_cover, "url": video_url}
-
 
     def get(self):
         try:
-            self.get_imu()
-            self.get_dytk()
-            res = self.get_infor()
+            if not self.get_video_num():
+                return None
+            # print(self.video_num)
+            if not self.get_video_info():
+                return None
         except:
             # 报错返回 None
             return None
         else:
-            return res
+            # print(self.video_info)
+            return self.get_info()
 
 
 if __name__ == '__main__':
-    dys = DYSpider(share_url="https://v.douyin.com/cCcyF5/")
-    print(dys.get())
+    dys = DYSpider(share_url="https://v.douyin.com/eqaknNr/").get()
+    print(dys)
